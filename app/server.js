3d4c853645acdbc191075ed0b3d106ad0bd644a1
lonk <lonk@live.fr>
2015-03-02 22:50:34 +0100

    Add a miniproxy to server.js to communicate to the API without CORS

--- app/server.js
+++ app/server.js
@@ -1,9 +1,31 @@
 var static = require('node-static');
+var http = require('http');
 
 var file = new static.Server('./public');
 
-require('http').createServer(function (request, response) {
+http.createServer(function (request, response) {
     request.addListener('end', function () {
-        file.serve(request, response);
+        file.serve(request, response, function (err, result) {
+            if (err) {
+            	var urlContent = request.url.split('/');
+            	if(urlContent[1] == 'api') {
+                    response.writeHead(200, {"Content-Type": "text/plain"});
+                    http.get("http://127.0.0.1:8081"+request.url, function(api_res) {
+                        var body = '';
+                        api_res.on('data', function(d) {
+                            body += d;
+                        });
+                        api_res.on('end', function() {
+                            response.end(body);
+                        });
+                    });
+            	} else {
+    	            console.log("Error serving " + request.url + " - " + err.message);
+
+    	            response.writeHead(err.status, err.headers);
+    	            response.end("Error");
+                }
+            }
+        });
     }).resume();
 }).listen(8082);
